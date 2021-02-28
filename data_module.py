1a3ff20c90950324a76569a5d7bfb3b2e82f649f
Claire Tang <claire.smurfs@gmail.com>
2021-02-28 09:11:02 -0800

    Lightning based code structure

---
+++ data_module.py
@@ -0,0 +1,70 @@
+from glob import glob
+import os
+
+import pytorch_lightning as pl
+import torch
+from torch.utils.data import DataLoader, random_split
+
+from dataset import Dataset
+
+class DataModule(pl.LightningDataModule):
+
+    def __init__(self, data_dir, batch_size=1, train_transform=None, val_transform=None, test_transform=None):
+        super().__init__()
+        self.img_ext = '.jpg'
+        self.mask_ext = '.png'
+        self.num_classes = 1
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.train_transform = train_transform
+        self.val_transform = val_transform
+        self.test_transform = test_transform
+        self.train_ids, self.val_ids, self.test_ids = None, None, None
+
+    def prepare_data(self):
+        # This method is used to define the processes that are meant to be 
+        # performed by only one GPU. It’s usually used to handle the task of
+        # downloading the data. 
+        pass
+
+    def setup(self, stage=None):
+        # This method is used to define the process that is meant to be performed by all the available GPU. 
+        # It’s usually used to handle the task of loading the data. 
+        img_files = glob(os.path.join(data_dir, 'images', '*.jpg'))
+        img_ids = [os.path.splitext(os.path.basename(p))[0] for p in img_files]
+        train_size = int(len(img_ids) * 0.9)
+        val_size = len(img_ids) - train_size
+        self.train_ids, self.val_ids = random_split(
+            img_ids, [train_size, val_size], generator=torch.Generator().manual_seed(41))
+
+    def train_dataloader(self):
+        return self._create_dataloader(self.train_ids, self.train_transform)
+
+    def val_dataloader(self):
+        return self._create_dataloader(self.val_ids, self.val_transform)
+
+    def test_dataloader(self):
+        raise NotImplementedError
+
+    def _create_dataloader(self, img_ids, transform):
+        dataset = Dataset(
+            img_ids,
+            os.path.join(self.data_dir, 'images'),
+            os.path.join(self.data_dir, 'masks'),
+            self.img_ext,
+            self.mask_ext,
+            self.num_classes)
+
+        return torch.utils.data.DataLoader(
+            dataset,
+            self.batch_size,
+            shuffle=True,
+            drop_last=True)
+
+if __name__ == '__main__':
+    data_dir = 'data/0420/lung'
+    batch_size = 4
+    data_module = DataModule(data_dir, batch_size)
+    data_module.prepare_data()
+    data_module.setup()
+    data_module.train_dataloader()
---
+++ entry.py
@@ -0,0 +1,117 @@
+import argparse
+import archs
+import losses
+
+from utils import str2bool
+from train import train_entry
+from test import test_entry
+from two_step_test import two_step_test_entry
+
+ARCH_NAMES = list(archs.__dict__.keys())
+LOSS_NAMES = list(losses.__dict__.keys())
+LOSS_NAMES.append('BCEWithLogitsLoss')
+
+"""restrict GPU option"""
+# find most open GPU (default use 8 gpus)
+# gpu_list = get_default_gpus(4)
+# gpu_ids = ','.join(map(str, gpu_list))
+#
+# allocate GPU
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = gpu_ids
+#
+# #print("Allocated GPU %s" % gpu_ids)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--name', default=None, help='model name: (default: arch+timestamp)')
+    parser.add_argument('--epochs', default=300, type=int, metavar='N', help='number of total epochs to run')
+    parser.add_argument('-b', '--batch_size', default=16, type=int, metavar='N', help='mini-batch size (default: 16)')
+
+    # train/test
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--test', action='store_true')
+    parser.add_argument('--two_step_test', action='store_true')
+    parser.add_argument('--predict', action='store_true')
+    parser.add_argument('--seg_img', default='seg_img', choices=['seg_only', 'seg_img', 'gt', 'crop_img'])
+
+    # data
+    parser.add_argument('--save_split', action='store_true')
+    parser.add_argument('--load_split', action='store_true')
+
+    # model
+    parser.add_argument('--arch', '-a', metavar='ARCH', default='NestedUNet',
+                        help='model architecture: ' +
+                        ' | '.join(ARCH_NAMES) +
+                        ' (default: NestedUNet)')
+    parser.add_argument('--deep_supervision', default=False, type=str2bool)
+    parser.add_argument('--input_channels', default=3, type=int, help='input channels') # 1 or 3
+    parser.add_argument('--loss', default='BCEDiceLoss',
+                        choices=LOSS_NAMES,
+                        help='loss: ' +
+                        ' | '.join(LOSS_NAMES) +
+                        ' (default: BCEDiceLoss)')
+
+    # dataset
+    parser.add_argument('--dataset', default='0420', help='dataset name')
+    # parser.add_argument('--dataset', default='cc_ccii', help='dataset name')
+    parser.add_argument('--dataset', default='ieee8023', help='dataset name')
+    # parser.add_argument('--dataset', default='NSCLC', help='dataset name')
+    # parser.add_argument('--sub_dataset', default='lung_disease', help='sub_dataset name')
+    # parser.add_argument('--sub_dataset', default='disease', help='sub_dataset name')
+    # parser.add_argument('--sub_dataset', default='disease_segmented_NSCLC', help='sub_dataset name')
+    parser.add_argument('--sub_dataset', default='lung', help='sub_dataset name')
+    parser.add_argument('--img_ext', default='.jpg', help='image file extension')
+    parser.add_argument('--mask_ext', default='.png', help='mask file extension')
+
+    parser.add_argument('--num_classes', default=1, type=int, help='number of classes')
+    parser.add_argument('--input_w', default=512, type=int, help='image width')
+    parser.add_argument('--input_h', default=512, type=int, help='image height')
+
+    # optimizer
+    parser.add_argument('--optimizer', default='SGD',
+                        choices=['Adam', 'SGD'],
+                        help='loss: ' +
+                        ' | '.join(['Adam', 'SGD']) +
+                        ' (default: Adam)')
+    parser.add_argument('--lr', '--learning_rate', default=1e-3, type=float, metavar='LR', help='initial learning rate')
+    parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+    parser.add_argument('--weight_decay', default=1e-4, type=float, help='weight decay')
+    parser.add_argument('--nesterov', default=False, type=str2bool, help='nesterov')
+
+    # scheduler
+    parser.add_argument('--scheduler', default='CosineAnnealingLR',
+                        choices=['CosineAnnealingLR', 'CosineAnnealingWarmRestarts', 'ReduceLROnPlateau', 'MultiStepLR', 'ConstantLR'])
+    parser.add_argument('--min_lr', default=1e-5, type=float, help='minimum learning rate')
+    parser.add_argument('--factor', default=0.1, type=float)
+    parser.add_argument('--patience', default=10, type=int)
+    parser.add_argument('--milestones', default='1,2', type=str)
+    parser.add_argument('--gamma', default=2/3, type=float)
+    parser.add_argument('--early_stopping', default=-1, type=int, metavar='N', help='early stopping (default: -1)')
+    
+    parser.add_argument('--num_workers', default=4, type=int)
+
+    config = parser.parse_args()
+
+    return config
+
+
+if __name__ == '__main__':
+    config = vars(parse_args())
+
+    if config['name'] is None:
+        if config['deep_supervision']:
+            config['name'] = '%s_%s_%s_%s_wDS' % (config['dataset'], config['sub_dataset'], config['arch'], config['loss'])
+        else:
+            config['name'] = '%s_%s_%s_%s_woDS' % (config['dataset'], config['sub_dataset'], config['arch'], config['loss'])
+
+    if config['train']:
+        # train
+        iou = train_entry(config, ix_sum=10)
+        # print(iou)
+    elif config['test']:
+        # test
+        test_entry(config)
+    elif config['two_step_test']:
+        two_step_test_entry(config)
