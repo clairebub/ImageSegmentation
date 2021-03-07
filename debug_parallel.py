670053909c94ae122f58199380b8c5f973bd4f6e
Claire Tang <claire.smurfs@gmail.com>
2021-03-07 10:44:40 -0800

    figured data parallel only happens during forward pass

--- debug_parallel.py
+++ debug_parallel.py
@@ -1,9 +1,15 @@
+import argparse
+import logging
 import os
 
 import matplotlib.pyplot as plt
 import torch 
 import torch.nn as nn 
+import pytorch_lightning as pl
 import torchvision
+
+import utils
+
 
 class RandomDataset(torch.utils.data.Dataset):
 
@@ -12,11 +18,39 @@ class RandomDataset(torch.utils.data.Dataset):
         self.data = torch.randn(num_inputs, input_size)
 
     def __getitem__(self, index):
+        logging.debug(f'data_loader worker info: {torch.utils.data.get_worker_info()}')
         return self.data[index]
 
     def __len__(self):
         return self.len
     
+
+class DataModule(pl.LightningDataModule):
+
+    def __init__(self):
+        super().__init__()
+        self.train_ids, self.val_ids, self.test_ids = None, None, None
+
+    def prepare_data(self):
+        # This method is used to define the processes that are meant to be 
+        # performed by only one GPU. It’s usually used to handle the task of
+        # downloading the data. 
+        pass
+
+    def setup(self, stage=None):
+        # This method is used to define the process that is meant to be performed by all the available GPU. 
+        # It’s usually used to handle the task of loading the data. 
+        pass
+
+    def train_dataloader(self):
+        pass
+
+    def val_dataloader(self):
+        pass
+
+    def test_dataloader(self):
+        raise NotImplementedError
+
 
 class Model(nn.Module):
 
@@ -26,28 +60,43 @@ class Model(nn.Module):
 
     def forward(self, input):
         output = self.fc(input)
-        #print(f"In Model: input.shape={input.shape}, output.shape={output.shape}")
+        logging.info(f"In Model: device={torch.cuda.current_device()}, input.shape={input.shape}, output.shape={output.shape}")
         return output
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_epochs', default=3, type=int, metavar='N', help='max number of epochs')
+    parser.add_argument('--num_workers', default=os.cpu_count(), type=int, metavar='N', help='number of data loader workers')
+    parser.add_argument('--num_gpus', default=torch.cuda.device_count(), type=int, metavar='N', help='number of gpus')
+    parser.add_argument('--input_size', default=2, type=int, metavar='N')
+    parser.add_argument('--output_size', default=4, type=int, metavar='N')
+    parser.add_argument('--num_inputs', default=1000, type=int, metavar='N', help='number of input samples')
+    parser.add_argument('-b', '--batch_size', default=16, type=int, metavar='N', help='batch size')
+    parser.add_argument('--pin_memory', default=True, type=utils.str2bool)
+    config = vars(parser.parse_args())
 
-    input_size = 2
-    output_size = 4
-    num_inputs = 100
-    batch_size = 16
+    logging.basicConfig(level=logging.INFO)
+    logging.info("=" * 40)
+    for k in sorted(config):
+        logging.info(f'{k}={config[k]}')
+    logging.info("=" * 40)
 
-    targets = torch.rand(batch_size, output_size)
-    dataset = RandomDataset(input_size, num_inputs)
+    targets = torch.rand(config['batch_size'], config['output_size'])
+    dataset = RandomDataset(config['input_size'], config['num_inputs'])
     data_loader = torch.utils.data.DataLoader(
         dataset,
-        batch_size=batch_size, 
-        num_workers=4,
+        batch_size=config['batch_size'],
+        num_workers=config['num_workers'],
+        pin_memory=config['pin_memory'],
         shuffle=True, 
         drop_last=True)
-    model = Model(input_size, output_size)
+    model = Model(config['input_size'], config['output_size'])
     model = nn.DataParallel(model)
-
+    if config['num_gpus'] > 0:
+        model = model.cuda()
+        targets = targets.cuda()
+   
     optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
     criterion = nn.MSELoss()
     # # Print model's state_dict
@@ -61,13 +110,13 @@ if __name__ == '__main__':
     #     print(var_name, "\t", optim.state_dict()[var_name])
     # print()
 
-    max_epochs = 3
-    for epoch in range(max_epochs):
-        print(f"epoch={epoch}")
-        if torch.cuda.is_available():
-            print(f"on {torch.cuda.current_device()}")
+    for epoch in range(config['max_epochs']):
+        logging.info(f"epoch={epoch}")
         for inputs in data_loader:
+            logging.info(f'batch inputs.shape={inputs.shape}')
+            inputs = inputs.cuda()
             outputs = model(inputs)
+            logging.info(f'batch outputs.shape={outputs.shape}')
             loss = criterion(outputs, targets)
             model.zero_grad()
             loss.backward()
